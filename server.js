e33d264e9296484471f42bb623e4d55b6f3874d3
PONGSATHORN UPAN <pongsathorn.upan@scb.co.th>
2023-06-30 15:18:39 +0700

    add path

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 let http = require('http');
 
 http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello NodeJS from Muzikzz Org');
+    if (req.url == '/') { //check the URL of the current request
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    }
+    else if (req.url == "/hello") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Hello NodeJS from Muzikzz Org.</p></body></html>');
+        res.end();
+    }
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.end('Hello NodeJS from Muzikzz Org');
 }).listen(1234, () => console.log('Server running on localhost:1234...')) ;
