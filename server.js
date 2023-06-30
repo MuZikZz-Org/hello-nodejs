9b1cc0d9ddfbcb3dc976779feb91b7c77d86d52e
PONGSATHORN UPAN <pongsathorn.upan@scb.co.th>
2023-06-30 15:24:03 +0700

    add else

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ http.createServer(function(req,res){
         res.write('<html><body><p>Hello NodeJS from Muzikzz Org.</p></body></html>');
         res.end();
     }
+    else
+        res.end('Invalid Request!');
     // res.writeHead(200, {'Content-Type': 'text/html'});
     // res.end('Hello NodeJS from Muzikzz Org');
 }).listen(1234, () => console.log('Server running on localhost:1234...')) ;
